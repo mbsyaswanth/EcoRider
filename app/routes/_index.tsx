fa631f7f63da4749e3b091cee0505d91029760fa
mbsyaswanth <mbs.yaswanth@gmail.com>
2024-05-13 20:03:18 +0530

    added homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,70 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "EcoRider | Home" },
     { name: "description", content: "Welcome to Remix!" }
   ];
 };
 
+const homeMenu = [
+  {
+    label: "Find Ride",
+    path: "/find-ride"
+  },
+  {
+    label: "Offer Ride",
+    path: "/offer-ride"
+  },
+  {
+    label: "Active Ride",
+    path: "/active-ride"
+  },
+  {
+    label: "Publish Requirement",
+    path: "/post-requirement"
+  }
+];
+
+const ActionLink = ({ label, path }: { label: string; path: string }) => {
+  return (
+    <Link
+      to={path}
+      className="flex basis-0 justify-center items-center text-lg text-center text-grey font-medium px-4 py-10 min-w-32 rounded-2xl shadow-[0_4px_48px_0_rgba(147,147,147,0.7)]"
+    >
+      {label}
+    </Link>
+  );
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex flex-col bg-almostWhite h-screen pt-4">
+      <header className="flex justify-between px-4 py-2 mb-6">
+        <img src="favicon.svg" alt="logo" />
+        <div className="flex items-center gap-5">
+          <button>
+            <img src="bullhorn.svg" alt="notifications" />
+          </button>
+          <button>
+            <img src="gear.svg" alt="notifications" />
+          </button>
+        </div>
+      </header>
+
+      <div className="px-4 py-2 mb-4">
+        <h1 className="text-xl text-grey font-medium">Hi Yaswanth!</h1>
+        <h4 className="text-base text-grey">
+          Discover rides or start driving.
+        </h4>
+      </div>
+
+      <div className="flex flex-wrap justify-between items-stretch content-start gap-9 bg-white h-full shadow-lighttop rounded-t-[50px] p-10">
+        {homeMenu.map(({ label, path }) => (
+          <ActionLink key={label} label={label} path={path} />
+        ))}
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,19 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    colors: {
+      primary: "#EF8203",
+      white: "#FFFFFF",
+      almostWhite: "#ECEDE5",
+      grey: "#44443D",
+      lgrey: "#44443D",
+      vlgrey: "#B4B1AE"
+    },
+    extend: {
+      boxShadow: {
+        lighttop: "0 18px 32px 2px rgba(0, 0, 0, 0.24)"
+      }
+    }
   },
   plugins: []
 } satisfies Config;
